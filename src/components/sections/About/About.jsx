a7331f577ec44195a5b19c443d0827d1a0a0a845
Alejandra <alegarpre@gmail.com>
2026-08-20 18:53:29 -0700

    Agregar ilustracion personal a la seccion Sobre mi
    Layout de dos columnas en desktop (avatar enmarcado + texto), apilado
    en mobile. El fondo blanco de la ilustracion se resuelve con una
    tarjeta oscura a su alrededor, consistente con el resto del sitio.

--- src/components/sections/About/About.jsx
+++ src/components/sections/About/About.jsx
@@ -1,5 +1,6 @@
 import SectionHeading from '../../SectionHeading/SectionHeading'
 import { useLanguage } from '../../../i18n/LanguageContext'
+import avatar from '../../../assets/avatar.png'
 import styles from './About.module.css'
 
 function About() {
@@ -8,9 +9,16 @@ function About() {
   return (
     <section id="sobre-mi" className={styles.about}>
       <SectionHeading number="01" title={t.about.heading} />
-      <p>{t.about.paragraph1}</p>
-      <p>{t.about.paragraph2}</p>
-      <p>{t.about.paragraph3}</p>
+      <div className={styles.layout}>
+        <div className={styles.avatarFrame}>
+          <img src={avatar} alt="Ilustración de Alejandra" className={styles.avatar} />
+        </div>
+        <div className={styles.text}>
+          <p>{t.about.paragraph1}</p>
+          <p>{t.about.paragraph2}</p>
+          <p>{t.about.paragraph3}</p>
+        </div>
+      </div>
     </section>
   )
 }
